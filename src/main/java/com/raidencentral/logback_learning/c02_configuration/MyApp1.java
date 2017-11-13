8dafed0da5caf22b96bfe31edf3a723c8e274640
raiden <peckwood@users.noreply.github.com>
2017-11-13 20:09:14 +0800

    minor

---
+++ src/main/java/com/raidencentral/logback_learning/c02_configuration/MyApp1.java
@@ -0,0 +1,16 @@
+package com.raidencentral.logback_learning.c02_configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyApp1 {
+	final static Logger logger = LoggerFactory.getLogger(MyApp1.class);
+	
+	public static void main(String[] args) {
+		logger.info("Entering application");
+		
+		Foo foo = new Foo();
+		foo.doIt();
+		logger.info("Existing application");
+	}
+}
